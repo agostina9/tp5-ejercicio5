1b07c3a7ff91e6c74ab94171300689413b055956
Agostina <lozagostina@gmail.com>
2022-08-14 20:56:42 -0300

    agrego funcionalidad del cronometro

---
+++ js/cronometro.js
@@ -0,0 +1,38 @@
+let temporizaodr= document.getElementById('tiempo');
+   let btnEmpezar=document.getElementById('btn-empezar');
+   let btnParar= document.getElementById('btn-parar');
+   let btnReset= document.getElementById('btn-reset');
+  
+    let tiempo=0;
+    let intervalo=0;
+    let verificador=false;
+evento();
+
+function evento(){
+btnEmpezar.addEventListener('click', iniciar);
+btnParar.addEventListener('click', parar);
+btnReset.addEventListener('click', reset);
+}
+function iniciar(){
+    if(verificador==false){
+        intervalo=setInterval(function(){
+            tiempo +=0.01;
+            temporizador.innerHTML=tiempo.toFixed(2);
+        },10);
+        verificador=true;
+    }
+}
+ function parar(){
+    verificador=false
+     clearInterval(intervalo);
+    
+ }
+ function reset(){
+verificador=false;
+tiempo=0;
+temporizador.innerHTML= tiempo + '00:00:00.00';
+ clearInterval(intervalo);
+ }
+
+
+    
